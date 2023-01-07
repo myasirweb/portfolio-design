9c9e0f3cfc4df6394703c7e17524103f28a684f8
Muhammad Yasir <119052144+myasirweb@users.noreply.github.com>
2023-01-07 19:40:54 +0500

    initial

---
+++ style.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+            // ...............for the Navbar to appear on scroll..................//
+
+    $(window).scroll(function(){
+
+        if(this.scrollY > 200){
+
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }        
+
+    });
+
+                //  ...........typing Animation..........//
+
+            var typed = new Typed(".typing-hero",{
+                strings:[
+                    "Designer", "Animator", "Developer", "Freelancer"
+                ],
+                typeSpeed: 100,
+                backSpeed: 60,
+                loop: true
+
+            });
+
+            var typed = new Typed(".typing-about",{
+                strings:[
+                    "Designer", "Animator", "Developer", "Freelancer"
+                ],
+                typeSpeed: 100,
+                backSpeed: 60,
+                loop: true
+
+            });
+
+            // owl port styling Animation
+            
+           $('.carousel').owlCarousel({
+            
+            margin: 20,
+            loop: true,
+            navigation: true,
+            autoplay: true,
+            autoplayTimeout: 1000,
+            autoplayHoverPause: true,
+
+            responsive: {
+                0:{
+                    items: 1,
+                    nav: false,
+                },
+                600: {
+                    items: 2,
+                    nav: false,
+                
+                },
+                1000: {
+                    items: 3,
+                    nav: false,
+                }
+            }
+
+           });
+
+});
